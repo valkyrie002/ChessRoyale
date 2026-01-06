7e32bb8ad8087b67949bbaf624998baa96f7634f
Joel Villarreal II <joel.villa2329@gmail.com>
2026-01-05 19:30:07 -0700

    board constructor

--- src/GameLogic/Components/Piece.java
+++ src/GameLogic/Components/Piece.java
@@ -6,6 +6,9 @@ import java.util.List;
 import java.util.Set;
 
 public interface Piece {
+    // TODO, have a get position, but no setPosition (do we want pieces tracking their own position,
+    // it may be better to have board do that?)
+    //TODO: pieces should be associated with player (add constructor to all Pieces which takes in int)
     Coordinate getPosition();
     Set<Coordinate> getMovement();
     Set<Coordinate> getAttack();
